7702a5c7d91c415c015aec2030c069e407fff95f
Thaylan Conceição <tc.thaylan@gmail.com>
2020-07-31 00:47:02 -0300

    alteracoes no create pub

--- src/functions/pub/create.js
+++ src/functions/pub/create.js
@@ -1,24 +1,6 @@
 import Pub from '../../models/Pub'
-import Client from '../../models/Client'
-import validateEmail from '../../utils/validadeEmail'
 
 export const createPub = async (req, res) => {
-  const { email, password } = req.body
-
-  if (!validateEmail(email)) {
-    return res.status(400).send({ error: 'Email inválido, digite um formato válido' })
-  }
-
-  const pubEmailExists = await Pub.findOne({ email })
-  if (pubEmailExists) {
-    return res.status(400).send({ error: 'Este email já existe, utilize outro' })
-  }
-
-  const clientEmailExists = await Client.findOne({ email })
-  if (clientEmailExists) {
-    return res.status(400).send({ error: 'Este email já existe, utilize outro' })
-  }
-
   const pub = new Pub(req.body)
   await pub.save((err) => {
     if (err) {
